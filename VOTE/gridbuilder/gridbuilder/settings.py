d6a107fa8e68ef51057c506cca979ea0e5a098d5
laurentdebeuf <laurent.debeuf@infocura.be>
2026-06-18 15:04:52 +0200

    railway deployment

--- VOTE/gridbuilder/gridbuilder/settings.py
+++ VOTE/gridbuilder/gridbuilder/settings.py
@@ -146,13 +146,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'planner/static'),
-)
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'planner/static/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
