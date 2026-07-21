9bc3401a37f804f5fcabc420d1c0af08f3893a23
laurentdebeuf <laurent.debeuf@infocura.be>
2026-07-21 19:33:35 +0200

    vote and moderator to railway

--- VOTE/moderator_app/moderator_app/settings.py
+++ VOTE/moderator_app/moderator_app/settings.py
@@ -181,7 +181,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
